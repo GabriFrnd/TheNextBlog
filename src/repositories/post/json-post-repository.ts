92752bb89daf1a15499c5a4faeca9edb05c6ca00
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-09 19:28:16 -0300

    🟢 Feat: função para simular tempo de loading na tela (simulateWait + setTimeout)

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -13,7 +13,14 @@ const JSON_POSTS_FILE_PATH = resolve(
   'posts.json',
 ); /* Caminho para o arquivo 'posts.json' */
 
+const SIMULATE = 5000;
+
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE <= 0) return;
+    await new Promise(resolve => setTimeout(resolve, SIMULATE));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> { /* Método privado para leitura de arquivo 'posts.json' */
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
     const parsedJson = JSON.parse(jsonContent);
@@ -23,11 +30,15 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> { /* Método que retorna todos os posts (array) */
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
 
   async findById(id: string): Promise<PostModel> { /* Método que retorna apenas um post baseado no ID */
+    await this.simulateWait();
+
     const posts = await this.findAll();
     const post = posts.find(post => post.id === id);
 
