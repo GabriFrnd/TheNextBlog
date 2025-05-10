45c0dd006f8af988de68efe8fb7bf392458a2f70
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-10 18:05:54 -0300

    🟢 Feat: criação de novo componente (Container) para abrigar os conteúdos do projeto na tela

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,13 @@
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+  return (
+    <div className='bg-slate-50 min-h-screen text-slate-900'>
+      <div className='mx-auto max-w-screen-lg px-8'>
+        {children}
+      </div>
+    </div>
+  );
+}
