3887c4e9afcae4c850203efdb6c62c99223912ab
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-10 22:10:43 -0300

    🟣 Styles: novas medidas de texto ajustadas à responsividade de telas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,11 +5,11 @@ export function Header() {
     <header>
       <h1
         className={clsx(
-          'font-extrabold py-5 text-4xl/snug',
-          'sm:py-7 sm:text-5xl/snug',
-          'md:py-9 md:text-6xl/snug',
-          'lg:py-11 lg:text-7xl/snug',
-          'xl:py-13 xl:text-8xl/snug',
+          'font-extrabold py-5 text-3xl/snug',
+          'sm:py-7 sm:text-4xl/snug',
+          'md:py-9 md:text-5xl/snug',
+          'lg:py-11 lg:text-6xl/snug',
+          'xl:py-13 xl:text-7xl/snug',
         )}
       >
         <a href='#'>The Blog</a>
