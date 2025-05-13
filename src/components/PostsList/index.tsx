4e8e623d08a900a6d1b082121fb38e9f7a7ab8d8
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-13 11:15:18 -0300

    🟢 Feat: criação da grid responsiva para os posts com Tailwind CSS

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,12 +1,46 @@
 import { postRepository } from '@/repositories/post';
+import { PostImageCover } from '../PostImageCover';
+
+import { PostHeading } from '../PostHeading';
+import clsx from 'clsx';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
 
   return (
-    <div>
+    <div className={clsx('gap-8', 'grid grid-cols-1', 'sm:grid-cols-2', 'lg:grid-cols-3')}>
       {posts.map(post => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div className={clsx('flex flex-col', 'gap-8', 'group')} key={post.id}>
+            <PostImageCover
+              linkProps={{
+                href: `/post/${post.slug}`,
+              }}
+              imageProps={{
+                src: post.coverImageUrl,
+                height: 720,
+                width: 1200,
+                alt: post.title,
+              }}
+            />
+            <div
+              className={clsx('flex flex-col', 'gap-4', 'sm:justify-center')}
+            >
+              <time
+                className={clsx('block', 'text-slate-600 text-sm/tight')}
+                dateTime={post.createdAt}
+              >
+                {post.createdAt}
+              </time>
+
+              <PostHeading href='#' as='h2'>
+                {post.title}
+              </PostHeading>
+
+              <p>{post.excerpt}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
