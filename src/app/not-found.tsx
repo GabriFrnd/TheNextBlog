db59f44914d69c381b136d77e0f7ba4219fde9cb
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-16 21:09:11 -0300

    🟢 Feat: página não encontrada para erro 404 (not found)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={clsx(
+        'bg-slate-900',
+        'flex',
+        'itens-center justify-center',
+        'mb-16',
+        'min-h-[100px]',
+        'p-8',
+        'rounded-2xl',
+        'text-center text-slate-100',
+      )}
+    >
+      <div>
+        <h1 className={clsx('font-bold', 'mb-2', 'text-3xl')}>404</h1>
+        <p className={clsx('font-bold', 'text-1xl')}>Essa página não pôde ser encontrada.</p>
+      </div>
+    </div>
+  );
+}
