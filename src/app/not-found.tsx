77d074732ce952236cccb36bc17ae5ee773fd36a
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-22 12:26:56 -0300

    🔵 Refactor: re-aproveitamento de componente (error message) para exibir mensagem de erro 404 (not-found.tsx)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,11 @@
-import clsx from 'clsx';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className={clsx(
-          'bg-slate-900',
-          'flex',
-          'itens-center justify-center',
-          'mb-16',
-          'min-h-[100px]',
-          'p-8',
-          'rounded-2xl',
-          'text-center text-slate-100',
-        )}
-      >
-        <div>
-          <h1 className={clsx('font-bold', 'mb-2', 'text-3xl')}>404</h1>
-          <p className={clsx('font-bold', 'text-1xl')}>
-            Essa página não pôde ser encontrada.
-          </p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle='Página não encontrada'
+      contentTitle='404'
+      content='Essa página não pôde ser encontrada.'
+    />
   );
 }
