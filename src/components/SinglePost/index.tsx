6ca28ac1d10112a75cb55238916ddd96123d585a
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-22 11:19:06 -0300

    🟢 Feat: renderização de conteúdo dos posts da página

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,4 +1,11 @@
 import { findPostBySlug } from '@/lib/post/queries';
+import { PostHeading } from '../PostHeading';
+
+import { PostDate } from '../PostDate';
+import { SafeMarkdown } from '../SafeMarkdown';
+
+import Image from 'next/image';
+import clsx from 'clsx';
 
 type SinglePostProps = {
   slug: string;
@@ -8,8 +15,24 @@ export async function SinglePost({ slug }: SinglePostProps) {
   const post = await findPostBySlug(slug);
 
   return (
-    <div>
-      <p>{post.title}</p>
-    </div>
+    <article className={clsx('mb-16')}>
+      <header className={clsx('flex flex-col', 'gap-4', 'mb-4')}>
+        <Image
+          className={clsx('rounded-xl')}
+          src={post.coverImageUrl}
+          height={720}
+          width={1200}
+          alt={post.title}
+        />
+
+        <PostHeading href={`/post/${post.slug}`}>{post.title}</PostHeading>
+        <p>
+          {post.author} | <PostDate dateTime={post.createdAt} />
+        </p>
+      </header>
+
+      <p className={clsx('mb-4', 'text-slate-600 text-xl')}>{post.excerpt}</p>
+      <SafeMarkdown markdown={post.content} />
+    </article>
   );
 }
