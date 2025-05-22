1244747882053c64fd10017137526bb43945639a
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-22 12:24:45 -0300

    🟢 Feat: criação de novo componente (error message) para exibir mensagens de erro ao usuário

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,38 @@
+import clsx from 'clsx';
+
+type ErrorMessageProps = {
+  pageTitle: string;
+  contentTitle: string;
+  content: React.ReactNode;
+};
+
+export default function ErrorMessage({
+  pageTitle,
+  contentTitle,
+  content,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div
+        className={clsx(
+          'bg-slate-900',
+          'flex',
+          'itens-center justify-center',
+          'mb-16',
+          'min-h-[100px]',
+          'p-8',
+          'rounded-2xl',
+          'text-center text-slate-100',
+        )}
+      >
+        <div>
+          <h1 className={clsx('font-bold', 'mb-2', 'text-3xl')}>
+            {contentTitle}
+          </h1>
+          <div className={clsx('font-bold', 'text-1xl')}>{content}</div>
+        </div>
+      </div>
+    </>
+  );
+}
