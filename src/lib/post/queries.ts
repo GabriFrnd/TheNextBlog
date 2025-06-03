f087b2ab43d898e44fb90abbfe1294f62712144b
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-06-02 21:59:38 -0300

    🟢 Feat: cache de funções (Data Cache) com Unstable Cache

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,20 +1,26 @@
 import { postRepository } from '@/repositories/post';
+import { unstable_cache } from 'next/cache';
+
 import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
-export const findAllPublicPosts = cache(
-  async () => await postRepository.findAllPublic(),
-);
+export const findAllPublicPosts = unstable_cache(cache(async () => {
+  return await postRepository.findAllPublic();
+}), ['posts'], {
+  tags: ['posts'],
+});
 
 export const findPostById = cache(
   async (id: string) => await postRepository.findById(id),
 );
 
-export const findPostBySlug = cache(async (slug: string) => {
+export const findPostBySlug = (slug: string) => unstable_cache(cache(async (slug: string) => {
   const post = await postRepository
     .findBySlugPublic(slug)
     .catch(() => undefined);
   if (!post) notFound();
 
   return post;
-});
+}), ['posts'], {
+  tags: [`post-${slug}`],
+})(slug);
