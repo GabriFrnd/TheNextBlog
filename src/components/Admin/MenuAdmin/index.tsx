05372f0882dda83723deb51c42a97a8b8739baa2
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-06-17 10:10:00 -0300

    🟣 Style: estilização do menu administrativo com Tailwind CSS

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,13 +1,78 @@
+'use client';
+
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
 import Link from 'next/link';
 
 export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  const navigationClasses = clsx(
+    'bg-slate-900 text-slate-100',
+    'flex flex-col',
+    'mb-10',
+    'rounded-lg',
+    'sm:flex-row sm:flex-wrap sm:h-auto sm:overflow-visible',
+    !isOpen && 'h-10',
+    !isOpen && 'overflow-hidden',
+  );
+
+  const linkClasses = clsx(
+    'flex',
+    'cursor-pointer',
+    'gap-2 h-10 px-4 shrink-0',
+    'hover:bg-slate-800 transition',
+    'items-center justify-start',
+    'rounded-lg',
+    '[&>svg]:h-[20px] [&>svg]:w-[20px]',
+  );
+
+  const buttonClasses = clsx(linkClasses, 'sm:hidden', 'text-blue-200');
+
   return (
-    <nav>
-      <a href='/' target='_blank'>
+    <nav className={navigationClasses}>
+      <button
+        className={buttonClasses}
+        onClick={() => setIsOpen(state => !state)}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
+
+      <a className={linkClasses} href='/' target='_blank'>
+        <HouseIcon />
         Home
       </a>
 
-      <Link href='/admin/post'>Posts</Link>
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post/new'>
+        <PlusIcon />
+        Criar Post
+      </Link>
     </nav>
   );
 }
