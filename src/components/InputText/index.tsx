c4a1785287e6583661c0c53b8601ed454c959fbc
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-06-19 11:42:03 -0300

    🟢 Feat: criação de novo componente (InputText) para utilização de campos de texto da aplicação com variações

---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import { useId } from 'react';
+
+type InputTextProps = {
+  label?: string;
+} & React.ComponentProps<'input'>;
+
+export function InputText({ label = '', ...props }: InputTextProps) {
+  const id = useId(); /* Gerador de ID único */
+
+  return (
+    <div className={clsx('flex flex-col', 'gap-2')}>
+      {label && <label htmlFor={id}>{label}</label>}
+
+      <input
+        {...props}
+        className={clsx(
+          'bg-white',
+          'focus:ring-blue-600',
+          'disabled:bg-slate-200 disabled:cursor-not-allowed disabled:placeholder-slate-500 disabled:text-slate-400',
+          'outline-0',
+          'placeholder-slate-400',
+          'px-2 py-1',
+          'read-only:bg-slate-200',
+          'ring-slate-400 ring-2',
+          'rounded transition',
+          'text-base/normal',
+          props.className,
+        )}
+        id={id}
+      />
+    </div>
+  );
+}
