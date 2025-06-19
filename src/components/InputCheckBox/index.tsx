a65ee08e3632f8c0e932e278b224dec0103899a5
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-06-19 12:18:33 -0300

    🟢 Feat: criação de novo componente (InputCheckBox)

---
+++ src/components/InputCheckBox/index.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx';
+import { useId } from 'react';
+
+type InputCheckBoxProps = {
+  label?: string;
+  type?: 'checkbox';
+} & React.ComponentProps<'input'>;
+
+export function InputCheckBox({ label = '', type = 'checkbox', ...props }: InputCheckBoxProps) {
+  const id = useId(); /* Gerador de ID único */
+
+  return (
+    <div className={clsx('flex', 'gap-3', 'items-center')}>
+      <input
+        {...props}
+        className={clsx(
+          'focus:ring-blue-600 focus:ring-2',
+          'h-4 w-4',
+          'outline-none',
+          props.className
+        )}
+        id={id}
+        type={type}
+      />
+
+      {label && <label htmlFor={id}>{label}</label>}
+    </div>
+  );
+}
