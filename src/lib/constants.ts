900bc3fccf22499de6e1899fbd70138d6c4f2a45
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-06-26 10:17:11 -0300

    🟢 Feat: updates no arquivo de constantes para armazenar URL e nome de diretório da pasta de imagens

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 export const SIMULATE = 100;
 export const IMAGE_UPLOADER_MAX_SIZE = 921600;
+
+export const IMAGE_UPLOAD_DIRECTORY = 'uploads';
+export const IMAGE_SERVER_URL = 'http://localhost:3000/uploads';
