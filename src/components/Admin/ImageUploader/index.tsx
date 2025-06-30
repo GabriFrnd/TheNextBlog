7719abc9e9e9241c15a21171c43b24f4360bbbfc
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-06-30 10:58:30 -0300

    🟢 Feat: criação de novo componente (ImageUploader) para upload de imagens para posts com validações no front-end

---
+++ src/components/Admin/ImageUploader/index.tsx
@@ -0,0 +1,102 @@
+'use client';
+
+import { Button } from '@/components/Button';
+import { ImageUpIcon } from 'lucide-react';
+
+import { useRef, useState, useTransition } from 'react';
+import { IMAGE_UPLOADER_MAX_SIZE } from '@/lib/constants';
+
+import { toast } from 'react-toastify';
+import { uploadImageAction } from '@/actions/upload/upload-image-action';
+
+import clsx from 'clsx';
+
+export function ImageUploader() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
+  const [imageUrl, setImageUrl] = useState('');
+
+  function handleChooseFile() {
+    const fileInput = fileInputRef.current;
+
+    if (!fileInput) return;
+    fileInput.click();
+  }
+
+  function handleChange() {
+    toast.dismiss();
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+    const formData = new FormData();
+
+    if (!fileInput) {
+      setImageUrl('');
+      return;
+    }
+
+    if (!file) {
+      setImageUrl('');
+      return;
+    }
+
+    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+      const maxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+      toast.error(`Imagem grande. Tamanho máximo permitido: ${maxSize} KB.`);
+
+      fileInput.value = '';
+      setImageUrl('');
+
+      return;
+    }
+
+    formData.append('file', file);
+
+    startTransition(async () => {
+      const result = await uploadImageAction(formData);
+
+      if (result.error) {
+        toast.error(result.error);
+        fileInput.value = '';
+
+        setImageUrl('');
+        return;
+      }
+
+      setImageUrl(result.url);
+      toast.success('Imagem enviada com sucesso!');
+    });
+
+    fileInput.value = '';
+  }
+
+  return (
+    <div className={clsx('flex flex-col', 'gap-4 py-4')}>
+      <Button className={clsx('self-start')} type='button' onClick={handleChooseFile} disabled={isUploading}>
+        <ImageUpIcon />
+        Enviar Imagem
+      </Button>
+
+      {!!imageUrl && (
+        <div className={clsx('flex flex-col', 'gap-4')}>
+          <p>
+            <b>URL da imagem:</b> {imageUrl}
+          </p>
+
+          {/* eslint-disable-next-line */}
+          <img className={clsx('rounded-lg')} src={imageUrl} alt='Imagem' />
+        </div>
+      )}
+
+      <input
+        className={clsx('hidden')}
+        type='file'
+        name='file'
+        accept='image/*'
+        ref={fileInputRef}
+        onChange={handleChange}
+        disabled={isUploading}
+      />
+    </div>
+  );
+}
