2507ab7691e7061f0e624ed2580858c5c07544d7
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-09 10:51:38 -0300

    🟢 Feat: páginas com rotas (estática e dinâmica) para criação e edição de posts

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,4 +1,17 @@
+import { ManagePostForm } from '@/components/Admin/ManagePostForm';
+import { Metadata } from 'next';
+
+import { findPostByIdAdmin } from '@/lib/post/queries/admin';
+import { notFound } from 'next/navigation';
+
+import { makePublicPostFromDb } from '@/dto/post/dto';
+import clsx from 'clsx';
+
 export const dynamic = 'force-dynamic'; /* Rota dinâmica */
+
+export const metadata: Metadata = {
+  title: 'Editar Post',
+};
 
 type AdminPostIdPageProps = {
   params: Promise<{
@@ -6,7 +19,22 @@ type AdminPostIdPageProps = {
   }>;
 };
 
-export default async function AdminPostIdPage({ params }: AdminPostIdPageProps) {
+export default async function AdminPostIdPage({
+  params,
+}: AdminPostIdPageProps) {
   const { id } = await params;
-  return <div>Admin: post ID page ({id})</div>;
+  const post = await findPostByIdAdmin(id).catch(() => undefined);
+
+  if (!post) notFound();
+
+  const publicPost = makePublicPostFromDb(post);
+
+  return (
+    <div className={clsx('flex flex-col', 'gap-6')}>
+      <h1>
+        <b>Editar Post</b>
+      </h1>
+      <ManagePostForm mode='update' publicPost={publicPost} />
+    </div>
+  );
 }
--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,18 @@
+import { ManagePostForm } from '@/components/Admin/ManagePostForm';
+import { Metadata } from 'next';
+import clsx from 'clsx';
+
 export const dynamic = 'force-dynamic'; /* Rota dinâmica */
 
+export const metadata: Metadata = {
+  title: 'Criar Novo Post',
+}
+
 export default async function AdminNewPostPage() {
-  return <div>Admin: new post page</div>;
+  return (
+    <div className={clsx('flex flex-col', 'gap-6')}>
+      <h1><b>Criar Novo Post</b></h1>
+      <ManagePostForm mode='create' />
+    </div>
+  );
 }
